7466241c47b179d7e27c2e30265d08863788a1ad
Shreya <128481198+Shreyasingh120@users.noreply.github.com>
2025-03-29 19:16:21 +0530

    create a vite + react file

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Code from "./Code"
+function App() {
+
+
+  return (
+    <>
+    <Code/>
+   <h1>Hello world</h1>
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Code.jsx
@@ -0,0 +1,7 @@
+function Code(){
+    return (
+        <h2>I a returning</h2>
+    )
+}
+
+export default Code;
