f141c7509aa7441fec5001bc18e90ed88eb1256c
Shreya <128481198+Shreyasingh120@users.noreply.github.com>
2025-03-29 19:14:47 +0530

    created a reactfile

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Code from "./Code";
+
+function App() {
+
+  return (
+    <>
+    <Code/>
+    <h1>hello world</h1>
+    </>
+  );
+
+}
+
+export default App;
---
+++ 01basicreact/src/Code.js
@@ -0,0 +1,7 @@
+function Code(){
+    return (
+        <h3>Hello my name is shreya kumari</h3>
+    );
+}
+
+export default Code;
